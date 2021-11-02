bbb40cffaf5e32ab93baeea63b53c1f7fa7e214e
Neil.Chuang <Neil.Chuang@tymphany.com>
2021-11-02 10:37:54 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import bybit
+import time
+import os
+
+
+api_key = 'WjHRjCYO8AUUlBr5pH'
+api_secret = 'wfJpsLBUTunyNpI8ANcxI6aw7JdZ7tnJPRA3'
+
+client = bybit.bybit(test=True, api_key=api_key, api_secret=api_secret)
+
+
+os.system('pause')
