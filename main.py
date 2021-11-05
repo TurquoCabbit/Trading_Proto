c15f74842365f160f7886b5fa173e7c60585fa6b
TurquoCabbit <night5916lan@gmail.com>
2021-11-06 00:23:24 +0800

    Add cfg file load feature and fix some bug

--- main.py
+++ main.py
@@ -1,21 +1,11 @@
-import decimal
-from hmac import digest_size
-from tkinter.constants import E
 from pybit import HTTP
 from time import sleep
 import os
+import json
 import random
 random.seed()
-import math
 
 import key
-
-Token_List = ['BTCUSDT', 'ETHUSDT', 'DOTUSDT', 'SOLUSDT', 'BITUSDT', 'AVAXUSDT', 'FTTUSDT']  #from loaded cfg
-operate_USDT = 1000
-Leverage = 20
-TP_percentage = 100
-SL_percentage = 10
-Max_operate_position = 5
 
 class Symbol:
     def __init__(self, Symbol, tick_size, qty_step) -> None:
@@ -27,10 +17,20 @@ class Symbol:
         self.leverage = Leverage
         self.tpsl_mode = 'Full'
 
+class Open:
+    def __init__(self) -> None:
+        self.ID = 0
+        self.sym = ''
+        self.price = 0
+        self.qty = 0
+        self.TP = 0
+        self.SL = 0
+
 Symbol_List = []
 
 qty_step_list = []
 
+Token_List = []
 
 
 current_position_qty = 0
@@ -80,11 +80,49 @@ def price_trim(price, tick, up = 0):
     
     return price / pow(10, digi)
 
+##############################################################################################################################
+Cfg_missing = True
+file_list = os.listdir()
+for i in file_list:
+    if i.endswith('.json'):
+        file = open(i)
+        Cfg_missing = False
+        break
 
+if Cfg_missing:
+    System_Msg('Cfg.json file missing')
+    print('cfg.json example : ')
+    print('{\n\t\"Test_net\" : ,\n\t\"Max_operate_position\" : ,\n\t\"operate_USDT\" : ,\n\t\"Leverage\" : ,')
+    print('\t"TP_percentage\" : ,\n\t\"SL_percentage\" : ,\n\Trade_all\" : ,\n\t\"Token\" : [')
+    print('\t    "BTC\",\n\t    \"ETH\",\n\t    .. ,\n\t    .. ,\n\t]\n}')
+    
+    os.system('pause')
+    os._exit(0)
 
+else:
+    Cfg_data = json.load(file)
+    file.close()
+
+    Test = Cfg_data['Test_net']
+    Max_operate_position = Cfg_data['Max_operate_position']
+    operate_USDT = Cfg_data['operate_USDT']
+    Leverage = Cfg_data['Leverage']
+    TP_percentage = Cfg_data['TP_percentage']
+    SL_percentage = Cfg_data['SL_percentage']
+    Trade_all = Cfg_data['Trade_all']
+    
+
+    if not Trade_all:
+        for i in Cfg_data['Token']:
+            Token_List.append('{}USDT'.format(i))
+
+##############################################################################################################################
 ### Connect client
 try:
-    client = HTTP(key.host, api_key=key.api, api_secret=key.api_secret)
+    if Test:
+        client = HTTP(key.host_test, api_key=key.api, api_secret=key.api_secret)
+    else:
+        client = HTTP(key.host, api_key=key.api, api_secret=key.api_secret)
 except :
     Error_Msg('Connect Error')
     os.system('pause')
@@ -94,18 +132,24 @@ except :
 try:
     Symbol_querry = client.query_symbol()
     if not Symbol_querry['ret_code']:
-        for i in Token_List:
-            exist = 0
-            for j in Symbol_querry['result']:
-                if i == j['name']:
-                    exist = 1
-                    Symbol_List.append(Symbol(i, j['price_filter']['tick_size'],\
-                                                 j['lot_size_filter']['qty_step']))
-                    break
-            if not exist:
-                raise Exception('\"{}\" not exist in Symbol list'.format(i))
-            else:
+        if not Trade_all:
+            for i in Token_List:
                 exist = 0
+                for j in Symbol_querry['result']:
+                    if i == j['name']:
+                        exist = 1
+                        Symbol_List.append(Symbol(i, j['price_filter']['tick_size'], j['lot_size_filter']['qty_step']))
+                        break
+                if not exist:
+                    raise Exception('\"{}\" not exist in Symbol list'.format(i))
+                else:
+                    exist = 0
+        else:
+            for i in Symbol_querry['result']:
+                if i['name'].endswith('USDT'):
+                    # print('\t\t\"{}\",'.format(i['name'].split('USDT')[0]))
+                    Symbol_List.append(Symbol(i['name'], i['price_filter']['tick_size'], i['lot_size_filter']['qty_step']))
+        
     else:
         raise Exception('Return code :{}'.format(Symbol_querry['ret_code']))
 
@@ -141,73 +185,81 @@ while True:
                     i.opened = True
             else:
                 raise Exception('Querry Position Fail !!\tReturn code: {}\tReturn msg: {}'.format(position['ret_code'], position['ret_msg']))
+            sleep(0.25)
 
         print('Opened order : {}'.format(current_position_qty))
 
 
         ### Randonly open position
         if current_position_qty < Max_operate_position:
+            open = Open()
+
             # Random pick
-            open_ID = rand_symbol()
-            open_sym = Symbol_List[open_ID].Symbol
+            open.ID = rand_symbol()
+            open.sym = Symbol_List[open.ID].Symbol
 
             # Set Full Position TP/SL
-            if Symbol_List[open_ID].tpsl_mode != 'Full':
-                temp = client.full_partial_position_tp_sl_switch(symbol = open_sym, tp_sl_mode = 'Full')
+            if Symbol_List[open.ID].tpsl_mode != 'Full':
+                temp = client.full_partial_position_tp_sl_switch(symbol = open.sym, tp_sl_mode = 'Full')
                 # print(temp)
                 if temp['ret_code'] == 0:
                     raise Exception('Set Full Position TP/SL Error\tReturn code: {}\tReturn msg: {}'.format(temp['ret_code'], temp['ret_msg']))
+                Symbol_List[open.ID].tpsl_mode = 'Full'
 
             # Switch to isolated margin mode and set leverage
-            if Symbol_List[open_ID].isolate != True:
-                temp = client.cross_isolated_margin_switch(symbol = open_sym, is_isolated = True, buy_leverage = Leverage, sell_leverage = Leverage)
+            if Symbol_List[open.ID].isolate != True:
+                temp = client.cross_isolated_margin_switch(symbol = open.sym, is_isolated = True, buy_leverage = Leverage, sell_leverage = Leverage)
                 # print(temp)
                 if temp['ret_code']:
                     raise Exception('Set Margin mode Error\tReturn code: {}\tReturn msg: {}'.format(temp['ret_code'], temp['ret_msg']))
+                Symbol_List[open.ID].isolate = True
+                Symbol_List[open.ID].leverge = Leverage
 
-            #Modify leverage
-            if Symbol_List[open_ID].leverge != Leverage:
-                temp = client.set_leverage(symbol = open_sym, buy_leverage = Leverage, sell_leverage = Leverage)
+            # Modify leverage
+            if Symbol_List[open.ID].leverge != Leverage:
+                temp = client.set_leverage(symbol = open.sym, buy_leverage = Leverage, sell_leverage = Leverage)
                 if temp['ret_code']:
                     raise Exception('Set Leverage Error\tReturn code: {}\tReturn msg: {}'.format(temp['ret_code'], temp['ret_msg']))
+                Symbol_List[open.ID].leverge = Leverage
 
             # Querry price
-            temp = client.latest_information_for_symbol(symbol = open_sym)
+            temp = client.latest_information_for_symbol(symbol = open.sym)
             if temp['ret_code']:
                 raise Exception('Querry current price Error\tReturn code: {}\tReturn msg: {}'.format(temp['ret_code'], temp['ret_msg']))
             else:
-                open_price = (float)(temp['result'][0]['last_price'])
+                open.price = (float)(temp['result'][0]['last_price'])
                 
 
             # Calculate TP/SL
-            open_qty = qty_trim((operate_USDT * Leverage / open_price), Symbol_List[open_ID].qty_step)
-            open_TP = price_trim((1 + (TP_percentage / Leverage / 100)) * open_price, Symbol_List[open_ID].tick_size)
-            open_SL = price_trim((1 - (SL_percentage / Leverage / 100)) * open_price, Symbol_List[open_ID].tick_size, True)
+            open.qty = qty_trim((operate_USDT * Leverage / open.price), Symbol_List[open.ID].qty_step)
+            open.TP = price_trim((1 + (TP_percentage / Leverage / 100)) * open.price, Symbol_List[open.ID].tick_size)
+            open.SL = price_trim((1 - (SL_percentage / Leverage / 100)) * open.price, Symbol_List[open.ID].tick_size, True)
 
-            print('Open {} {} Order at {} with TP : {}, SL : {}'.format(open_qty, open_sym, open_price, open_TP, open_SL))
+            print('Open {} {} Order at {} with TP : {}, SL : {}'.format(open.qty, open.sym, open.price, open.TP, open.SL))
             
 
             # Open position
-            open_posi = client.place_active_order(symbol = open_sym,\
+            open.posi = client.place_active_order(symbol = open.sym,\
                                                 side = "Buy",\
                                                 order_type = "Market",\
-                                                qty = open_qty,\
+                                                qty = open.qty,\
                                                 time_in_force = "GoodTillCancel",\
                                                 reduce_only = False,\
                                                 close_on_trigger = False,\
-                                                take_profit = open_TP,\
-                                                stop_loss = open_SL)
+                                                take_profit = open.TP,\
+                                                stop_loss = open.SL)
 
-            if open_posi['ret_code'] != 0:
-                raise Exception('{} Order Create Fail !!\tReturn code: {}\tReturn msg: {}'.format(open_sym, open_posi['ret_code'], open_posi['ret_msg']))
-            elif open_posi['ret_msg'] != 'OK':
-                print('{} Order Create Successfully !!\tReturn msg: {}'.format(open_sym,open_posi['ret_msg']))
+            if open.posi['ret_code'] != 0:
+                raise Exception('{} Order Create Fail !!\tReturn code: {}\tReturn msg: {}'.format(open.sym, open.posi['ret_code'], open.posi['ret_msg']))
+            elif open.posi['ret_msg'] != 'OK':
+                print('{} Order Create Successfully !!\tReturn msg: {}'.format(open.sym,open.posi['ret_msg']))
             else:
-                print('{} Order Create Successfully !!'.format(open_sym))
+                print('{} Order Create Successfully !!'.format(open.sym))
             
-            sleep(10)
+            del open
+            sleep(30)
         else:
-            sleep(1)
+            sleep(60)
 
 
     except Exception as Err:
