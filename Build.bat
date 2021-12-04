f4a5d5da13c84a3c72cf4cef05abfc6b8faeacb7
Turquo_PC <night5916lan@gmail.com>
2021-12-04 16:52:54 +0800

    Modify release script

--- Build.bat
+++ Build.bat
@@ -1,6 +1,6 @@
 pyinstaller -F main.py
 copy /Y dist\main.exe output\main.exe
-copy /Y dist\main.exe ..\main.exe
+rem copy /Y dist\main.exe ..\main.exe
 call Clean.bat
 git add --all
 git status
--- Clean.bat
+++ Clean.bat
@@ -1,4 +1,8 @@
 rmdir /s /q __pycache__
 rmdir /s /q build
 rmdir /s /q dist
+rmdir /s /q archive
+rmdir /s /q log
+rmdir /s /q pnl
+
 del *.spec
